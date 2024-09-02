83d849014bdc3c95f837ab42cbc97023ee9b502e
Ravindra Yadav <yadavra2005@gmail.com>
2024-09-02 21:12:49 +0530

    add private and authroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import Auth from "./pages/auth/Auth";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/post/Dashboard";
 import Register from "./pages/auth/Register";
 import Profile from "./pages/profilePage/Profile";
@@ -11,19 +11,61 @@ import AllUsersPage from "./pages/users/AllUsersPage";
 
 function App() {
   const login = useSelector((state) => state.auth.login);
-  console.log("login", login);
+  // console.log("login", login);
+
+  const PrivateRoutes = ({ children }) => {
+    return login ? children : <Navigate to="/auth" />;
+  };
+
+  const AuthRoute = ({ children }) => {
+    return !login ? children : <Navigate to="/dashboard" />;
+  };
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<Auth />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
+        <Route
+          path="/auth"
+          element={
+            <AuthRoute>
+              <Auth />
+            </AuthRoute>
+          }
+        ></Route>
 
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoutes>
+              <Profile />
+            </PrivateRoutes>
+          }
+        ></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoutes>
+              <Dashboard />
+            </PrivateRoutes>
+          }
+        ></Route>
+
+        <Route
+          path="/communities"
+          element={
+            <PrivateRoutes>
+              <AllCommunicatiesPage />
+            </PrivateRoutes>
+          }
+        ></Route>
+        <Route path="/all-people" element={
+          <PrivateRoutes>
+            <AllUsersPage />
+          </PrivateRoutes>
+        }></Route>
+
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/communities" element={<AllCommunicatiesPage />}></Route>
-        <Route path="/all-people" element={<AllUsersPage />}></Route>
-        <Route path="*" element={<Auth />}></Route>
+        {/* <Route path="*" element={<Auth />}></Route> */}
       </Routes>
     </BrowserRouter>
   );
